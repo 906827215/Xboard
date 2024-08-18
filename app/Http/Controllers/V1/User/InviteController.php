a9290cd3292b76efbd560a14b7b6ece7e3b92555
Gavin.Q <906827215@qq.com>
2024-08-18 09:29:27 +0800

    1、添加邀请页面数据返回 2、添加check函数

--- app/Http/Controllers/V1/User/InviteController.php
+++ app/Http/Controllers/V1/User/InviteController.php
@@ -11,6 +11,7 @@ use App\Models\InviteCode;
 use App\Models\Order;
 use App\Models\User;
 use App\Utils\Helper;
+use DateTime;
 use Illuminate\Http\Request;
 
 class InviteController extends Controller
@@ -57,18 +58,112 @@ class InviteController extends Controller
         if (admin_setting('commission_distribution_enable', 0)) {
             $uncheck_commission_balance = $uncheck_commission_balance * (admin_setting('commission_distribution_l1') / 100);
         }
+
+        // 当前时间戳
+        $currentTimestamp = time();
+        // 当月开始时间戳
+        $currentMonthStart = strtotime(date('Y-m-01 00:00:00'));
+        // 当月结束时间戳
+        $currentMonthEnd = strtotime(date('Y-m-t 23:59:59'));
+        // 上月开始时间戳
+        $lastMonthStart = strtotime(date('Y-m-01 00:00:00', strtotime('first day of last month')));
+        // 上月结束时间戳
+        $lastMonthEnd = strtotime(date('Y-m-t 23:59:59', strtotime('last day of last month')));
+
+        # 老写法
+//            CommissionLog::where('invite_user_id', $user->id)
+//                ->whereBetween('created_at', [$currentMonthStart, $currentMonthEnd])
+//                ->whereIn('user_id', User::where('invite_user_id', $user->id)
+//                    ->whereBetween('created_at', [$currentMonthStart, $currentMonthEnd])
+//                    ->pluck('id'))
+//                ->distinct('userid')
+//                ->count('userid'),
+
         $stat = [
-            //已注册用户数
+            //已注册用户数 / 上月 / 当月
             (int)User::where('invite_user_id', $user->id)->count(),
-            //有效的佣金
+            (int)User::where('invite_user_id', $user->id)
+                ->whereBetween('created_at', [$lastMonthStart, $lastMonthEnd])
+                ->count(),
+            (int)User::where('invite_user_id', $user->id)
+                ->whereBetween('created_at', [$currentMonthStart, $currentMonthEnd])
+                ->count(),
+            # 充值新人数
+            CommissionLog::join('v2_user', 'v2_commission_log.user_id', '=', 'v2_user.id')
+                ->where('v2_commission_log.invite_user_id', $user->id)
+                ->whereBetween('v2_commission_log.created_at', [$lastMonthStart, $lastMonthEnd])
+                ->where('v2_user.invite_user_id', $user->id)
+                ->whereBetween('v2_user.created_at', [$lastMonthStart, $lastMonthEnd])
+                ->distinct('v2_commission_log.user_id')
+                ->count('v2_commission_log.user_id'),
+
+            CommissionLog::join('v2_user', 'v2_commission_log.user_id', '=', 'v2_user.id')
+                ->where('v2_commission_log.invite_user_id', $user->id)
+                ->whereBetween('v2_commission_log.created_at', [$currentMonthStart, $currentMonthEnd])
+                ->where('v2_user.invite_user_id', $user->id)
+                ->whereBetween('v2_user.created_at', [$currentMonthStart, $currentMonthEnd])
+                ->distinct('v2_commission_log.user_id')
+                ->count('v2_commission_log.user_id'),
+
+            //佣金
             (int)CommissionLog::where('invite_user_id', $user->id)
                 ->sum('get_amount'),
-            //确认中的佣金
-            $uncheck_commission_balance,
+            (int)CommissionLog::where('invite_user_id', $user->id)
+                ->whereBetween('created_at', [$lastMonthStart, $lastMonthEnd])
+                ->sum('get_amount'),
+            (int)CommissionLog::where('invite_user_id', $user->id)
+                ->whereBetween('created_at', [$currentMonthStart, $currentMonthEnd])
+                ->sum('get_amount'),
+
             //佣金比例
             (int)$commission_rate,
+            //确认中的佣金
+            $uncheck_commission_balance,
             //可用佣金
             (int)$user->commission_balance
+        ];
+        $data = [
+            'codes' => InviteCodeResource::collection($user->codes),
+            'stat' => $stat
+        ];
+        return $this->success($data);
+    }
+
+    public function check(Request $request)
+    {
+        $user = User::find($request->user['id'])
+            ->load(['codes' => fn($query) => $query->where('status', 0)]);
+        $start = $request->getParam('start');
+        $end = $request->getParam('end');
+        $startDateTime = new DateTime($start);
+        $endDateTime = new DateTime($end);
+        $startTimestamp = $startDateTime->getTimestamp();;
+        $endOfTimestamp = $endDateTime->getTimestamp();
+
+
+
+        # 首充
+        $pay_num = CommissionLog::join('v2_user', 'v2_commission_log.user_id', '=', 'v2_user.id')
+            ->where('v2_commission_log.invite_user_id', $user->id)
+            ->whereBetween('v2_commission_log.created_at', [$startTimestamp, $endOfTimestamp])
+            ->where('v2_user.invite_user_id', $user->id)
+            ->whereBetween('v2_user.created_at', [$startTimestamp, $endOfTimestamp])
+            ->distinct('v2_commission_log.user_id')
+            ->count('v2_commission_log.user_id');
+        # 新注册
+        $reg_num = User::where('invite_user_id', $user->id)
+            ->whereBetween('created_at', [$startTimestamp, $endOfTimestamp])
+            ->count();
+        $rate = number_format($pay_num / $reg_num * 100, 2);
+
+        $amount = (int)CommissionLog::where('invite_user_id', $user->id)
+            ->whereBetween('created_at', [$startTimestamp, $endOfTimestamp])
+            ->sum('get_amount');
+        $stat = [
+            $pay_num,
+            $reg_num,
+            $rate,
+            $amount
         ];
         $data = [
             'codes' => InviteCodeResource::collection($user->codes),

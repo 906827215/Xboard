2b1cf62c3efc42de10e4f41630bcf4767e7820d2
郭先森的独白-A-good-boy <906827215@qq.com>
2025-03-18 11:17:26 +0800

    1、添加游客接口-查询节点服务

--- app/Http/Routes/V1/GuestRoute.php
+++ app/Http/Routes/V1/GuestRoute.php
@@ -10,6 +10,8 @@ class GuestRoute
         $router->group([
             'prefix' => 'guest'
         ], function ($router) {
+            // server
+            $router->get ('/servers/fetch', 'V1\\Guest\\ServerController@fetch');
             // Plan
             $router->get ('/plan/fetch', 'V1\\Guest\\PlanController@fetch');
             // Telegram
